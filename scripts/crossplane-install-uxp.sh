4590ce2411debdd2d34cc4bf708a030247777536
Joost van der Griendt <joostvdg@gmail.com>
2022-12-06 15:26:25 +0100

    testing

--- scripts/crossplane-install-uxp.sh
+++ scripts/crossplane-install-uxp.sh
@@ -6,3 +6,5 @@ CROSSPLANE_NAMESPACE=${CROSSPLANE_NAMESPACE:-upbound-system}
 echo ">> Installing UXP - Universal Crossplane"
 up uxp install --set 'args={--enable-external-secret-stores}' ${UXP_VERSION}
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/component=cloud-infrastructure-controller --namespace ${CROSSPLANE_NAMESPACE}
+
+kubectl create clusterrolebinding crossplane-admin-binding --clusterrole cluster-admin --serviceaccount="upbound-system:crossplane" || true
