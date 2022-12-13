78c8d339a05e98eed14374b1e1f6f42880beca7e
Yannick <yfoeillet@vmware.com>
2022-12-13 08:40:22 +0100

    feat(postgres) Improve app call and perf

--- examples/postgresql/golang/pkg/config/config.go
+++ examples/postgresql/golang/pkg/config/config.go
@@ -4,61 +4,57 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/bzhtux/servicebinding/bindings"
 	"github.com/kelseyhightower/envconfig"
-	. "github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/mapstructure"
 	"github.com/vmware-tanzu/tanzu-application-platform-reference-service-packages/examples/postgresql/golang/models"
 	"gopkg.in/yaml.v2"
 )
 
-const (
-	DEFAULT_CONFIG_DIR = "/config"
-	pgConfigDir        = "PG_CONFIG_DIR"
-	pgConfigFile       = "postgres.yaml"
+var (
+	EnvConfigDir = strings.ToUpper(AppName) + "_CONFIG_DIR"
+	AppVersion   = "0.0.1"
+	AppPort      = 8080
 )
 
-type PGConfigDir struct {
-	root string
-}
+const (
+	DEFAULT_CONFIG_DIR    = "/config"
+	ConfigFile            = "config.yml"
+	AppName               = "gopg"
+	EnvServiceBindingRoot = "SERVICE_BINDING_ROOT"
+)
 
-type Config interface {
-	NewConfig()
-}
+type Conf models.Config
 
-type PGConfig models.PGSpec
-
-func GetConfigDir() *PGConfigDir {
-	CONFIG_DIR, exists := os.LookupEnv(pgConfigDir)
-	pgcd := &PGConfigDir{}
+func (cfg *Conf) GetConfigDir() *Conf {
+	cfg_dir, exists := os.LookupEnv(EnvConfigDir)
 	if !exists {
-		pgcd.root = DEFAULT_CONFIG_DIR
+		cfg.Dir.Root = DEFAULT_CONFIG_DIR
 	} else {
-		pgcd.root = CONFIG_DIR
+		cfg.Dir.Root = cfg_dir
 	}
-	return pgcd
+
+	return cfg
 }
 
-func (pgc *PGConfig) LoadConfigFromFile() error {
-	pgcd := GetConfigDir()
-	cfg, err := os.Open(filepath.Join(pgcd.root, pgConfigFile))
+func (cfg *Conf) LoadConfigFromFile() error {
+	config := cfg.GetConfigDir()
+	c, err := os.Open(filepath.Join(config.Dir.Root, ConfigFile))
 	if err != nil {
 		log.Printf("Error opening PostgreSQL config file: %s\n", err.Error())
 		return err
 	}
-	defer cfg.Close()
-	d := yaml.NewDecoder(cfg)
-	if err := d.Decode(&pgc); err != nil {
+	defer c.Close()
+	d := yaml.NewDecoder(c)
+	if err := d.Decode(&cfg); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (cfg *PGConfig) LoadConfigFromEnv() {
-	envconfig.Process("", cfg)
-}
-
-func (cfg *PGConfig) LoadConfigFromBindings(t string) error {
+func (cfg *Conf) LoadConfigFromBindings(t string) error {
 
 	b, err := bindings.NewBinding(t)
 	if err != nil {
@@ -66,16 +62,18 @@ func (cfg *PGConfig) LoadConfigFromBindings(t string) error {
 		return err
 	}
 
-	if err := Decode(b, &cfg); err != nil {
+	if err := mapstructure.Decode(b, &cfg); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (cfg *PGConfig) NewConfig() *PGConfig {
+func (cfg *Conf) NewConfig() {
+	// 1st : Load config from config file
 	cfg.LoadConfigFromFile()
+	// 2nd : Load config from bindings
 	cfg.LoadConfigFromBindings("postgresql")
-	cfg.LoadConfigFromEnv()
-	return cfg
+	// 3rd : Load config from env
+	envconfig.Process("", cfg.Database)
 }
