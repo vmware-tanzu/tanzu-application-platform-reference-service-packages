32140ddfcf7e7d177dda88efca32eb2850c6d181
Joost van der Griendt <joostvdg@gmail.com>
2022-12-14 16:00:10 +0100

    tf provider has been replaced with the official one

--- scripts/crossplane-e2e-install-tf-provider.sh
+++ scripts/crossplane-e2e-install-tf-provider.sh
@@ -1,5 +1,13 @@
-up controlplane provider install xpkg.upbound.io/crossplane-contrib/provider-terraform:v0.4.0 || true
+cat <<EOF | kubectl apply -f -
+apiVersion: pkg.crossplane.io/v1
+kind: Provider
+metadata:
+  name: provider-terraform
+spec:
+  package: xpkg.upbound.io/upbound/provider-terraform:v0.2.0
+EOF
 
-kubectl wait --for=condition="Healthy" providers.pkg.crossplane.io crossplane-contrib-provider-terraform
+kubectl wait --for=condition="Healthy" providers.pkg.crossplane.io provider-terraform
+
 
 sleep 1
