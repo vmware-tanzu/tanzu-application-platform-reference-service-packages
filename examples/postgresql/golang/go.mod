983a1af4c86a66ada091560838ab5690cd5a0d98
Yannick <yfoeillet@vmware.com>
2022-12-15 10:57:03 +0100

    fix(examples) Fix some issues with unused files and fix some issues from last failed merge

--- examples/postgresql/golang/go.mod
+++ examples/postgresql/golang/go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/bzhtux/servicebinding/bindings v0.0.0-20221026140015-49543914590f
 	github.com/gin-gonic/gin v1.8.1
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/mitchellh/mapstructure v1.5.0
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/postgres v1.4.5
 	gorm.io/gorm v1.24.1
@@ -32,6 +31,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
--- examples/redis/golang/models/config.go
+++ examples/redis/golang/models/config.go
@@ -1,9 +1,5 @@
 package models
 
-// const (
-// 	CONFIG_DIR = "/config"
-// )
-
 type Config struct {
 	Dir struct {
 		Root string
@@ -15,12 +11,12 @@ type Config struct {
 		Port    int
 	}
 	Database struct {
-		Host       string `yaml:"host" envconfig:"REDIS_HOST"`
-		Port       int    `yaml:"port" envconnfig:"REDIS_PORT"`
-		Username   string `yaml:"username" envconfig:"REDIS_USERNAME"`
-		Password   string `yaml:"password" envconfig:"REDIS_PASSWORD"`
-		DB         string `yaml:"database" envconfig:"REDIS_DB"`
-		SSL 	   bool   `yaml:"sslenabled" envconfig:"REDIS_SSL"`
-		Type       string `yaml:"type" envconfig:"REDIS_TYPE"`
+		Host     string `yaml:"host" envconfig:"REDIS_HOST"`
+		Port     int    `yaml:"port" envconnfig:"REDIS_PORT"`
+		Username string `yaml:"username" envconfig:"REDIS_USERNAME"`
+		Password string `yaml:"password" envconfig:"REDIS_PASSWORD"`
+		DB       string `yaml:"database" envconfig:"REDIS_DB"`
+		SSL      bool   `yaml:"sslenabled" envconfig:"REDIS_SSL"`
+		Type     string `yaml:"type" envconfig:"REDIS_TYPE"`
 	}
 }
