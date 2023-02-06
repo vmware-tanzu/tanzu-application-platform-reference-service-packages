9c0395868e9a696a75b6bd3dbb46f52002a2a1c0
Matteo Magni <matteo.magni@gmail.com>
2023-02-06 10:42:52 +0100

    fix terraform provider's resources api group

--- scripts/crossplane-install-tf-provider.sh
+++ scripts/crossplane-install-tf-provider.sh
@@ -5,4 +5,4 @@ PROVIDER_NAME="upbound-provider-terraform"
 up controlplane provider install xpkg.upbound.io/upbound/provider-terraform:v0.2.0 --name ${PROVIDER_NAME} || true
 
 kubectl wait --for=condition="Healthy" providers.pkg.crossplane.io ${PROVIDER_NAME}
-kubectl wait --for=condition=Available=True apiservices.apiregistration.k8s.io v1alpha1.tf.crossplane.io
+kubectl wait --for=condition=Available=True apiservices.apiregistration.k8s.io v1beta1.tf.upbound.io
